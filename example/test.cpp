c5bd487465636133ed515029639c5b75e52b726b
eszdman <eszdman2015@gmail.com>
2022-09-10 00:28:07 +0300

    Initial commit

---
+++ example/test.cpp
@@ -0,0 +1,9 @@
+#include "excerpt.h"
+
+typedef float fp_t; // floating point type for all the operations
+//typedef double fp_t; // floating point type for all the operations
+
+int main(void)
+{
+
+}
